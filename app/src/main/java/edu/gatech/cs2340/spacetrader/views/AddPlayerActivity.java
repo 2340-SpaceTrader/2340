9e5ee19ae48f96f8b015217690b3081b1c6274db
Tiana Hom <thom6@gatech.edu>
2019-02-16 14:36:17 -0500

    Player Activity

---
+++ app/src/main/java/edu/gatech/cs2340/spacetrader/views/AddPlayerActivity.java
@@ -0,0 +1,21 @@
+package edu.gatech.cs2340.spacetrader.views;
+
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class AddPlayerActivity extends AppCompatActivity {
+    private EditText name;
+    private EditText pilotPts;
+    private EditText engrPts;
+    private EditText tradePts;
+    private EditText fighterPts;
+    private Spinner difficultySpinner;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_player);
+    }
+}
