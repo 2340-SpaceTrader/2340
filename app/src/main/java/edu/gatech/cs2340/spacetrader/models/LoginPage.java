07563bfb8ab5ed68e26ca0af031638c173a97c8f
Tu Nguyen <tnguyen496@gatech.edu>
2019-02-16 16:59:39 -0500

    FrontPage and LoginPage

---
+++ app/src/main/java/edu/gatech/cs2340/spacetrader/models/LoginPage.java
@@ -0,0 +1,17 @@
+public class LoginPase extends AppCompatActivity {
+
+	private Button logOut;
+	private Button createNewGame;
+	private Button loadGame;
+
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+
+		logOut = findViewById(R.id._____);
+		createNewGame = findViewById(R.id._____);
+		loadGame = findViewById(R.id._____);
+
+
+	}
+
+}
