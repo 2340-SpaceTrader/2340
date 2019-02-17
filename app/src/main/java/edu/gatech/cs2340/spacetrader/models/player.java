921ca8672f088a02803597e4367aee4a55a8faac
Luan Nguyen <nguyenluan2603@gmail.com>
2019-02-17 13:14:06 -0500

    Finish M5

--- app/src/main/java/edu/gatech/cs2340/spacetrader/models/player.java
+++ app/src/main/java/edu/gatech/cs2340/spacetrader/models/player.java
@@ -2,6 +2,7 @@ package edu.gatech.cs2340.spacetrader.models;
 
 import java.util.ArrayList;
 
+
 public class player {
     private String name;
     private int skillPoints;
@@ -10,54 +11,100 @@ public class player {
     private gameDifficulty gameDifficulty;
     private ArrayList SPAllocation;
 
+    /**
+     * Constructor of a player
+     * @param name
+     * @param gameDifficulty
+     */
+
     public player(String name, gameDifficulty gameDifficulty) {
         this.name = name;
         this.gameDifficulty = gameDifficulty;
         skillPoints = 16;
         credits =1000;
         GnatSpaceShip = true;
-        SPAllocation = new ArrayList<Integer>(3);
+        SPAllocation = new ArrayList<Integer>(4);
     }
 
     public String getName() {
         return name;
     }
+
     public int getSkillPoints() {
         return skillPoints;
     }
+
     public gameDifficulty getGameDifficulty() {
         return  gameDifficulty;
     }
+
     public ArrayList getSPAllocation() {
         return SPAllocation;
     }
+
+    public int getCredits() {
+        return credits;
+    }
+
+    public boolean getShip() {
+        return GnatSpaceShip;
+    }
+
     public void setName(String name) {
         this.name = name;
     }
+
     public void setSkillPoints(int skillPoints) {
         this.skillPoints = skillPoints;
     }
+
     public void setGameDifficulty(gameDifficulty gameDifficulty) {
         this.gameDifficulty = gameDifficulty;
     }
-    public boolean assertSP(ArrayList<Integer> list) {
+
+    public void setSPAllocation(ArrayList<Integer> list) {
+        SPAllocation = list;
+    }
+
+    /**
+     * To check for negative and number bigger than 16
+     * @param list
+     * @return boolean
+     */
+    public boolean assertNonNeg16(ArrayList<Integer> list) {
+        for(int i = 0; i < 4; i++) {
+            if (list.get(i) < 0 || list.get(i) > 16) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    /**
+     * To check for sum
+     * @param list
+     * @return boolean
+     */
+
+    public boolean assertSum16(ArrayList<Integer> list) {
         int sum = 0;
         for(int i = 0; i < 4; i++) {
-            if (list.get(i) < 0) {
-                throw new IllegalArgumentException("Input cannot be negative");
-            }
             sum += list.get(i);
         }
-        if (sum == 16) {
-            return true;
-        }
-        return false;
+        return sum == 16;
     }
+
+    /**
+     * To display player's attributes
+     * @return
+     */
+
     @Override
     public String toString() {
-        return String.format("Player's name: %s \n Game mode:  \n " +
-                "Pilot points: %d \n Fighter points: %d \n Trader points: %d \n Engineer points: %d \n"
-                , name, gameDifficulty, SPAllocation.get(0), SPAllocation.get(1),
+        return String.format("Player's name: %s \n Game mode: %s \n " +
+                "Credits: %d \n GnatSpaceShip: %s \n" +
+                " Pilot points: %d \n Fighter points: %d \n Trader points: %d \n Engineer points: %d \n"
+                , name, gameDifficulty.toString(), credits, GnatSpaceShip, SPAllocation.get(0), SPAllocation.get(1),
                 SPAllocation.get(2), SPAllocation.get(3));
     }
 }
--- app/src/main/java/edu/gatech/cs2340/spacetrader/views/AddPlayerActivity.java
+++ app/src/main/java/edu/gatech/cs2340/spacetrader/views/AddPlayerActivity.java
@@ -1,20 +1,27 @@
 package edu.gatech.cs2340.spacetrader.views;
-
-import android.widget.EditText;
-import android.widget.Spinner;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
-import android.view.View.OnClickListener;
-import android.support.v7.app.AppCompatActivity;
-import edu.gatech.cs2340.spacetrader.R;
-import edu.gatech.cs2340.spacetrader.models.gameDifficulty;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
-import edu.gatech.cs2340.spacetrader.models.player;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.Toast;
+
 import java.util.ArrayList;
 
+import edu.gatech.cs2340.spacetrader.MainActivity;
+import edu.gatech.cs2340.spacetrader.R;
+import edu.gatech.cs2340.spacetrader.models.gameDifficulty;
+import edu.gatech.cs2340.spacetrader.models.player;
+
+/**
+ * The Add player page
+ */
 public class AddPlayerActivity extends AppCompatActivity {
+
     private EditText name;
     private EditText pilotPts;
     private EditText engrPts;
@@ -28,15 +35,20 @@ public class AddPlayerActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_player);
 
-        name = findViewById(R.id.student_name_input);
-        pilotPts = findViewById(R.id.pilot_points_input);
-        engrPts = findViewById(R.id.engineer_points_input);
-        traderPts = findViewById(R.id.trader_points_input);
-        fighterPts = findViewById(R.id.fighter_points_input);
+        name = findViewById(R.id.player_name);
+        pilotPts = findViewById(R.id.pilot_point);
+        engrPts = findViewById(R.id.engineer_point);
+        traderPts = findViewById(R.id.trader_point);
+        fighterPts = findViewById(R.id.fighter_point);
         difficultySpinner = findViewById(R.id.difficulty_spinner);
-
+        Button okButton = findViewById(R.id.ok_button);
         Button exitButton = findViewById(R.id.exit_button);
-        exitButton.setOnClickListener(new OnClickListener() {
+
+        ArrayAdapter<gameDifficulty> difficultyAdapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_item,gameDifficulty.values());
+        difficultyAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        difficultySpinner.setAdapter(difficultyAdapter);
+
+        exitButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 finish();
@@ -44,24 +56,28 @@ public class AddPlayerActivity extends AppCompatActivity {
             }
         });
 
-        Button okButton = findViewById(R.id.ok_button);
-        okButton.setOnClickListener(new OnClickListener() {
+        okButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                // check to make sure player data is good - viewmodel
-                Log.i("Player data:\n", player.toString());
+                player = new player(name.getText().toString(), (gameDifficulty) difficultySpinner.getSelectedItem());
+                ArrayList<Integer> playerPts = new ArrayList<>(4);
+                playerPts.add(Integer.parseInt(pilotPts.getText().toString()));
+                playerPts.add(Integer.parseInt(fighterPts.getText().toString()));
+                playerPts.add(Integer.parseInt(traderPts.getText().toString()));
+                playerPts.add(Integer.parseInt(engrPts.getText().toString()));
+                player.setSPAllocation(playerPts);
+                if (!player.assertNonNeg16(playerPts)) {
+                    Toast.makeText(getApplicationContext(), "Input cannot be bigger than 16", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                if (!player.assertSum16(playerPts)) {
+                    Toast.makeText(getApplicationContext(), "Total skill points cannot be bigger than 16", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                System.out.println(player.toString());
             }
         });
 
-        name.setText(player.getName());
-        ArrayList playerPts = player.getSPAllocation();
-        pilotPts.setText(String.valueOf(playerPts.get(0)));
-        fighterPts.setText(String.valueOf(playerPts.get(1)));
-        traderPts.setText(String.valueOf(playerPts.get(2)));
-        engrPts.setText(String.valueOf(playerPts.get(3)));
-
-        ArrayAdapter<gameDifficulty> difficultyAdapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_item, gameDifficulty.values());
-        difficultyAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        difficultySpinner.setAdapter(difficultyAdapter);
     }
+
 }
